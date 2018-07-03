7756221dc96af6a7914973caaf438d19c110794d
vunguyen <you@example.com>
2018-07-04 00:14:45 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,80 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { PushNotificationIOS } from 'react-native';
+import Amplify from 'aws-amplify';
+import { PushNotification } from 'aws-amplify-react-native';
+import aws_exports from './aws-exports';
+
+Amplify.configure(aws_exports);
+PushNotification.configure(aws_exports);
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+
+  componentDidMount() {
+    PushNotification.onNotification((notification) => {
+      console.log('in app notification', notification);
+      // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
+     // notification.finish(PushNotificationIOS.FetchResult.NoData);
+    });
+
+    // gets the registration token`
+    PushNotification.onRegister((token) => {
+      console.log('in app registration', token);
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
